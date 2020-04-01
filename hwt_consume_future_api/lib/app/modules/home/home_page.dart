7f0c7cf7f4502dda6877b2bc6b105109e74da511
Bwolfs2 <bwolfnoob@gmail.com>
2020-04-01 09:50:52 -0300

    remove charts =/

--- hwt_consume_future_api/lib/app/modules/home/home_page.dart
+++ hwt_consume_future_api/lib/app/modules/home/home_page.dart
@@ -18,61 +18,61 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
   ///line
-  Widget _buildChartLine(context) {
-    var chartLine = ChartLine(
-      chartBeans: [
-        ChartBean(x: '12-01', y: 30),
-        ChartBean(x: '12-02', y: 25),
-        ChartBean(x: '12-03', y: 20),
-        ChartBean(x: '12-04', y: 22),
-        ChartBean(x: '12-05', y: 21),
-        ChartBean(x: '12-06', y: 18),
-        ChartBean(x: '12-07', y: 18),
-      ],
-      size: Size(MediaQuery.of(context).size.width,
-          MediaQuery.of(context).size.height / 5 * 1.6),
-      isCurve: true,
-      lineWidth: 1,
-      lineColor: Colors.yellow,
-      fontColor: Colors.black,
-      xyColor: Colors.black.withOpacity(.1),
-      shaderColors: [
-        Colors.red.withOpacity(0.3),
-        Colors.yellow.withOpacity(0.1)
-      ],
-      fontSize: 12,
-      yNum: 5,
-      isAnimation: true,
-      isReverse: false,
-      isCanTouch: true,
-      isShowPressedHintLine: true,
-      pressedPointRadius: 4,
-      pressedHintLineWidth: 0.5,
-      pressedHintLineColor: Colors.black,
-      duration: Duration(milliseconds: 1000),
-      backgroundColor: Colors.white,
-    );
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-      semanticContainer: true,
-      color: Colors.yellow.withOpacity(0.4),
-      child: chartLine,
-      clipBehavior: Clip.antiAlias,
-    );
-  }
+  // Widget _buildChartLine(context) {
+  //   var chartLine = ChartLine(
+  //     chartBeans: [
+  //       ChartBean(x: '12-01', y: 30),
+  //       ChartBean(x: '12-02', y: 25),
+  //       ChartBean(x: '12-03', y: 20),
+  //       ChartBean(x: '12-04', y: 22),
+  //       ChartBean(x: '12-05', y: 21),
+  //       ChartBean(x: '12-06', y: 18),
+  //       ChartBean(x: '12-07', y: 18),
+  //     ],
+  //     size: Size(MediaQuery.of(context).size.width,
+  //         MediaQuery.of(context).size.height / 5 * 1.6),
+  //     isCurve: true,
+  //     lineWidth: 1,
+  //     lineColor: Colors.yellow,
+  //     fontColor: Colors.black,
+  //     xyColor: Colors.black.withOpacity(.1),
+  //     shaderColors: [
+  //       Colors.red.withOpacity(0.3),
+  //       Colors.yellow.withOpacity(0.1)
+  //     ],
+  //     fontSize: 12,
+  //     yNum: 5,
+  //     isAnimation: true,
+  //     isReverse: false,
+  //     isCanTouch: true,
+  //     isShowPressedHintLine: true,
+  //     pressedPointRadius: 4,
+  //     pressedHintLineWidth: 0.5,
+  //     pressedHintLineColor: Colors.black,
+  //     duration: Duration(milliseconds: 1000),
+  //     backgroundColor: Colors.white,
+  //   );
+  //   return Card(
+  //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+  //     margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+  //     semanticContainer: true,
+  //     color: Colors.yellow.withOpacity(0.4),
+  //     child: chartLine,
+  //     clipBehavior: Clip.antiAlias,
+  //   );
+  // }
 
-  Map<DateTime, double> createLine2() {
-    Map<DateTime, double> data = {};
-    data[DateTime.now().subtract(Duration(minutes: 40))] = 13.0;
-    data[DateTime.now().subtract(Duration(minutes: 30))] = 24.0;
-    data[DateTime.now().subtract(Duration(minutes: 22))] = 39.0;
-    data[DateTime.now().subtract(Duration(minutes: 20))] = 29.0;
-    data[DateTime.now().subtract(Duration(minutes: 15))] = 27.0;
-    data[DateTime.now().subtract(Duration(minutes: 12))] = 9.0;
-    data[DateTime.now().subtract(Duration(minutes: 5))] = 35.0;
-    return data;
-  }
+  // Map<DateTime, double> createLine2() {
+  //   Map<DateTime, double> data = {};
+  //   data[DateTime.now().subtract(Duration(minutes: 40))] = 13.0;
+  //   data[DateTime.now().subtract(Duration(minutes: 30))] = 24.0;
+  //   data[DateTime.now().subtract(Duration(minutes: 22))] = 39.0;
+  //   data[DateTime.now().subtract(Duration(minutes: 20))] = 29.0;
+  //   data[DateTime.now().subtract(Duration(minutes: 15))] = 27.0;
+  //   data[DateTime.now().subtract(Duration(minutes: 12))] = 9.0;
+  //   data[DateTime.now().subtract(Duration(minutes: 5))] = 35.0;
+  //   return data;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -87,22 +87,22 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
           Observer(builder: (_) {
             return Column(
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AnimatedLineChart(
-                    AreaLineChart.fromDateTimeMaps([
-                      createLine2()
-                    ], [
-                      Colors.red.shade900
-                    ], [
-                      'C'
-                    ], gradients: [
-                      Pair(Colors.yellow.shade400, Colors.red.shade700)
-                    ]),
-                    key: UniqueKey(),
-                  ), //Unique key to force animations
-                ),
-                _buildChartLine(context),
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: AnimatedLineChart(
+                //     AreaLineChart.fromDateTimeMaps([
+                //       createLine2()
+                //     ], [
+                //       Colors.red.shade900
+                //     ], [
+                //       'C'
+                //     ], gradients: [
+                //       Pair(Colors.yellow.shade400, Colors.red.shade700)
+                //     ]),
+                //     key: UniqueKey(),
+                //   ), //Unique key to force animations
+                // ),
+                // _buildChartLine(context),
                 Expanded(
                   child: ListView.builder(
                     itemCount: controller.pokemons.length,
