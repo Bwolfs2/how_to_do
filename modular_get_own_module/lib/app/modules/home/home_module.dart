5f5f2d663696d85ed958cd3dc8175435d41d68e4
Vilson Dauinheimer <bwolfnoob@gmail.com>
2020-10-16 10:22:21 -0300

    Defining in a previous Module is having problem

--- modular_get_own_module/lib/app/modules/home/home_module.dart
+++ modular_get_own_module/lib/app/modules/home/home_module.dart
@@ -1,3 +1,6 @@
+import 'package:modular_get_own_module/app/modules/shared/some_interface.dart';
+import 'package:modular_get_own_module/app/modules/shared/third_some.dart';
+
 import 'home_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -7,6 +10,10 @@ class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
         $HomeController,
+        //TODO here is the problem
+        Bind<ISome>(
+          (i) => SomeThird(),
+        )
       ];
 
   @override
